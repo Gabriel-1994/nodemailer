c8ad7cf3fe281ff3ab038d304090b54604f363fc
Gabriel Nalbandian <gabriel.nalbandian94@gmail.com>
2021-09-27 20:24:31 +0300

    okay23456

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.route('/sendmail').post((req, res) => {
     let MessageBody = req.body['MessageBody'];
     let Subject = req.body['Subject'];
    
-    let htmlBody = '<p>' + MessageBody + '<img src = "' + Server + '/recipients/:' + Recipient + '>' + '</p>';
+    let htmlBody = '<p>' + MessageBody + '</p>' + '<img src = "' + Server + 'recipients/' + Recipient + '" style="display:none">';
     var mailOptions = {
       from: Sender,
       to: Recipient,
